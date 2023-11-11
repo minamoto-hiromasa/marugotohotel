53d58b6b10655b2c3fd9869071b33ea4821a4459
Takeshi Hatano <tkc@MacBook-Pro.local>
2023-11-11 20:25:33 +0900

    campaign logos

--- app/lib/footer.php
+++ app/lib/footer.php
@@ -2,13 +2,36 @@
 
 function getSponserContent() {
   $config = include get_template_directory() . '/app/config/variables.php';
-  $config['campaignPageSlug'];
-  echo '>>> ' . is_page('campaign');
-  echo '<br>';
   $page = get_page_by_path("general", OBJECT, "content-settings");
-  // var_dump(get_post_meta( $page->ID, 'sponser-icon'));
-  // var_dump(get_post_meta( $page->ID, 'sponser-link'));
-  // var_dump(get_post_meta( $page->ID, 'sponseruse'));
-  // echo wp_get_attachment_url($img);
-  // $sponserHtml = '<div class="sponsers">';
+  $icons = get_post_meta( $page->ID, 'sponser-icon');
+  $links = get_post_meta( $page->ID, 'sponser-link');
+  $forCampaigns = get_post_meta( $page->ID, 'for-campaign');
+  if (empty($icons)) {
+    return '';
+  }
+  $html = '<div class="sponsers">';
+  for ($i=0; $i < count($icons); $i++) {
+    $icon = $icons[$i];
+    $forCampaign = $forCampaigns[$i];
+    $link = $links[$i];
+    if (!is_page($config['campaignPageSlug']) && $forCampaign) {
+      continue;
+    }
+    if (empty($icon)) {
+      continue;
+    }
+    if (!empty($links)) {
+      $html .=
+        '<a href="' . $link . '" target="_blank"
+        rel="noopener noreferrer">';
+    }
+    $html .=
+      '<img src="' . wp_get_attachment_url($icon) . '"
+      alt="' . get_post_meta($icon, '_wp_attachment_image_alt', true) . '">';
+    if (!empty($links)) {
+      $html .= '</a>';
+    }
+  }
+  $html .= '</div>';
+  return $html;
 }
--- footer.php
+++ footer.php
@@ -23,15 +23,7 @@
 </div>
 <?php endif; ?>
 <footer id="footer">
-  <div class="sponsers">
-    <img src="<?php echo get_template_directory_uri()?>/assets/public/images/sponser-ensen-marugoto.jpg" width="84"
-      alt="沿線まるごと株式会社">
-    <img src="<?php echo get_template_directory_uri()?>/assets/public/images/sponser-satoyume.jpg" width="108"
-      alt="さとゆめ 故郷の夢をかたちに">
-    <img src="<?php echo get_template_directory_uri()?>/assets/public/images/sponser-jr.png" width="43" alt="JR東日本">
-    <img src="<?php echo get_template_directory_uri()?>/assets/public/images/sponser-tokyo-adventure-line.jpg"
-      width="50" alt="東京アドベンチャーライン">
-  </div>
+  <?php echo getSponserContent(); ?>
   <div class="brand">
     <ul>
       <?php
@@ -58,7 +50,6 @@
       </li>
       <?php endif ?>
     </ul>
-    <?php getSponserContent(); ?>
     <div class="copyright">Copyright © 2023 沿線まるごと株式会社. All Right Reserved.</div>
   </div>
 </footer>
